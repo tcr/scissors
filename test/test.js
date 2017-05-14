727c80be62eae15423c3648faec1feffd1022461
Christian Boulanger <info@bibliograph.org>
2017-05-14 17:04:14 +0200

    Added test files

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var scissors = require('./scissors');
+/* global describe, it */
+
+describe('Scissors', function() {
+  describe('#...()', function() {
+    it('should ...', function() {
+      
+
+    });
+  });
+});
