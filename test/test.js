ac909426ed289d61319a6ff98d7663d32531477e
Joseph Chen <joseph.chen@gmail.com>
2017-05-15 12:07:04 -0700

    Add more generic imageStream function which allows you to pass in page number and format for output, as well as the option to use simple rasterizer that skips temp file creation and bounding box code.

--- test/test.js
+++ test/test.js
@@ -39,4 +39,120 @@ describe('Scissors', function() {
       });
     });
   });
+
+  describe('#jpgStream()', function() {
+    it('should extract a single jpg page (using default rasterize)', function(done) {
+      var outfile = dir+'/_page1_default.jpg';
+      var dpi = 300;
+      var pageNum = 1;
+      var useSimpleRasterize = false;
+      try{ fs.unlinkSync(outfile); } catch(e){/**/}
+      scissors(pdf)
+      .jpgStream(dpi, pageNum, useSimpleRasterize).pipe(fs.createWriteStream(outfile))
+      .on('close', function(){
+        assert.equal(true,fs.existsSync(outfile));
+        done();
+      })
+      .on('error',function(err){
+        console.error(err.message);
+      });
+    });
+  });
+
+  describe('#pngStream()', function() {
+    it('should extract a single png page (using default rasterize)', function(done) {
+      var outfile = dir+'/_page1_default.png';
+      var dpi = 300;
+      var pageNum = 1;
+      var useSimpleRasterize = false;
+      try{ fs.unlinkSync(outfile); } catch(e){/**/}
+      scissors(pdf)
+      .pngStream(dpi, pageNum, useSimpleRasterize).pipe(fs.createWriteStream(outfile))
+      .on('close', function(){
+        assert.equal(true,fs.existsSync(outfile));
+        done();
+      })
+      .on('error',function(err){
+        console.error(err.message);
+      });
+    });
+  });
+
+  describe('#jpgStream()', function() {
+    it('should extract a single jpg page (using simple rasterize)', function(done) {
+      var outfile = dir+'/_page1_simple.jpg';
+      var dpi = 300;
+      var pageNum = 1;
+      var useSimpleRasterize = true;
+      try{ fs.unlinkSync(outfile); } catch(e){/**/}
+      scissors(pdf)
+      .jpgStream(dpi, pageNum, useSimpleRasterize).pipe(fs.createWriteStream(outfile))
+      .on('close', function(){
+        assert.equal(true,fs.existsSync(outfile));
+        done();
+      })
+      .on('error',function(err){
+        console.error(err.message);
+      });
+    });
+  });
+
+  describe('#pngStream()', function() {
+    it('should extract a single png page (using simple rasterize)', function(done) {
+      var outfile = dir+'/_page1_simple.png';
+      var dpi = 300;
+      var pageNum = 1;
+      var useSimpleRasterize = true;
+      try{ fs.unlinkSync(outfile); } catch(e){/**/}
+      scissors(pdf)
+      .pngStream(dpi, pageNum, useSimpleRasterize).pipe(fs.createWriteStream(outfile))
+      .on('close', function(){
+        assert.equal(true,fs.existsSync(outfile));
+        done();
+      })
+      .on('error',function(err){
+        console.error(err.message);
+      });
+    });
+  });
+
+  describe('#jpgStream()', function() {
+    it('should extract a single jpg page using crop box (using simple rasterize)', function(done) {
+      var outfile = dir+'/_page1_simple_crop_box.jpg';
+      var dpi = 300;
+      var pageNum = 1;
+      var useSimpleRasterize = true;
+      var useCropBox = true;
+      try{ fs.unlinkSync(outfile); } catch(e){/**/}
+      scissors(pdf)
+      .jpgStream(dpi, pageNum, useSimpleRasterize, useCropBox).pipe(fs.createWriteStream(outfile))
+      .on('close', function(){
+        assert.equal(true,fs.existsSync(outfile));
+        done();
+      })
+      .on('error',function(err){
+        console.error(err.message);
+      });
+    });
+  });
+
+  describe('#pngStream()', function() {
+    it('should extract a single png page using crop box (using simple rasterize)', function(done) {
+      var outfile = dir+'/_page1_simple_crop_box.png';
+      var dpi = 300;
+      var pageNum = 1;
+      var useSimpleRasterize = true;
+      var useCropBox = true;
+      try{ fs.unlinkSync(outfile); } catch(e){/**/}
+      scissors(pdf)
+      .pngStream(dpi, pageNum, useSimpleRasterize, useCropBox).pipe(fs.createWriteStream(outfile))
+      .on('close', function(){
+        assert.equal(true,fs.existsSync(outfile));
+        done();
+      })
+      .on('error',function(err){
+        console.error(err.message);
+      });
+    });
+  });
 });
